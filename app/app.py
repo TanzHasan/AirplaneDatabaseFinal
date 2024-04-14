485b6dfe9b52487d495283d3c2efcd2780a1dc4b
TanzHasan <tanzirhasanhasan@gmail.com>
2024-04-13 20:27:40 -0400

    Got the jinja rendering

--- app/app.py
+++ app/app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 import pymysql
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 app = Flask(__name__)
+
+mysql_config = {
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'db': os.getenv('DB_NAME'),
+    'charset': 'utf8mb4',
+    'cursorclass': pymysql.cursors.DictCursor
+}
+
+@app.route('/login_user', methods=['GET', 'POST'])
+def login_user():
+
+    connection = pymysql.connect(**mysql_config)
+    connection.close()
+
+    return render_template('login.html')
+
 @app.route('/')
 def home():
-    return
+    connection = pymysql.connect(**mysql_config)
+    connection.close()
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
