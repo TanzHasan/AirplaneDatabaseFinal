ca3792fcc5d7a138ef075142ae84874081b01c59
TanzHasan <tanzirhasanhasan@gmail.com>
2024-04-14 12:34:08 -0400

    Fixed insert.sql capitalization bug

--- sql_setup/insert.sql
+++ sql_setup/insert.sql
@@ -1,23 +1,23 @@
 insert into Airline values
 			('Jetblue');
 
-insert into airport 
+insert into Airport 
 values
-	('JFK', 'John F. Kennedy Airport', 'New York City','United States', 4, 'Both'),
-	('PVG', 'Shanghai international airport', 'Shanghai', 'China', 4, 'Both');
+	('JFK', 'John F. Kennedy', 'New York City','United States', 4, 'Both'),
+	('PVG', 'Shanghai Intl', 'Shanghai', 'China', 4, 'Both');
 
-insert into customers values
+insert into Customers values
 ("hi@gmail.com", "notgood", "alex", "ander", 370, "Jay Street Metrotech", "New York City", "New York", "11101", "123456789", "2026-12-17", "United States", "2001-11-11"),
 ("hello@gmail.com", "amazing", "bob", "ert", 371, "Jay Street Metroootech", "New York City", "New York", "11101", "123457891", "2026-10-17", "United States", "2003-12-15"),
 ("bye@gmail.com", "password123", "calvin", "hobbes", 372, "Jay Street Metroootech", "New York City", "New York", "11101", "132457891", "2026-10-17", "United States", "2003-12-15");
 
 
-insert into airplane values 
+insert into Airplane values 
 ("Jetblue", 12, 8, "Boeing 10", "Boeing", "2022-10-17", 2), 
 ("Jetblue", 13, 8, "Airbus 737", "Airbus", "2022-10-18", 2), 
 ("Jetblue", 14, 8, "Boeing 747", "Boeing", "2022-10-19", 2);
 
-insert into airlinestaff values 
+insert into AirlineStaff values 
 ("AliceB", "Jetblue", "Boeinglove", "Alice", "Boeing", "2022-10-17");
 
 insert into PhoneNumber values
@@ -26,7 +26,7 @@ insert into PhoneNumber values
 insert into EmailAddress values
 ("AliceB", "Jetblue", "boring@gmail.com");
 
-insert into flight values 
+insert into Flight values 
 ('Jetblue', 13, 102, '2023-06-01', '08:50:10', '2023-06-02', '11:35:00', 150.00, 'normal', true, 'PVG', 'JFK'),
 ('Jetblue', 14, 104, '2023-07-11', '11:50:10', '2023-07-12', '11:35:00', 150.00, 'normal', true, 'JFK', 'PVG'),
 ('Jetblue', 12, 101, '2023-06-01', '08:00:00', '2023-06-01', '10:30:00', 150.00, 'normal', true, 'PVG', 'JFK'),
